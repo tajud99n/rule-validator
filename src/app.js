ae23a6692f91a041f4531cf7ea5f02e99ad8ee26
tajud99n <giwa208@gmail.com>
2021-01-28 13:56:14 +0100

    base setup

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const { logger } = require("./config/logger");
+const { http_responder } = require("./utils/http_response");
 const BaseRouter = require("./routes");
 
 // Init express
@@ -21,18 +22,22 @@ app.use("/", BaseRouter);
 
 // handle errors
 app.all("/*", (req, res) => {
-	return http_responder.errorResponse(
-		res,
-		`${NOT_FOUND} - Not found`,
-		NOT_FOUND
+	return http_responder.sendResponse(
+        res,
+        null,
+        "not found",
+        "error",
+		400
 	);
 });
 
 app.use((err, req, res) => {
 	logger.error(JSON.stringify(err.stack));
-	return http_responder.errorResponse(
-		res,
-		err.message,
+	return http_responder.sendResponse(
+        res,
+        null,
+        err.message,
+        "error",
 		err.status || 500
 	);
 });
